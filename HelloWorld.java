36c486eb84db468b3e3e694c35b44d4b9a676569
TeacherGuy <kmcdaniel@pvnccdsb.on.ca>
2025-09-10 08:06:38 -0400

    Update HelloWorld.java
    Testing version control

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("Wow.   It's 2025 and Github Codespaces is not working.   How do we do "software" or "version" control?
     }
 }
 
